45dc29d2780d55717daa0aaa694845943b5f4ff4
Torgeir <torgeir@helgevold-consulting.com>
2014-01-25 11:45:39 -0500

    Refactoring of parser. Split out specific element parsers to new class

--- LambdaToXpath/ElementParser.cs
+++ LambdaToXpath/ElementParser.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using LambdaToXpath.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LambdaToXpath
 {
     public static class ElementParser
     {
+        private static string extractMethodArguments = @"(?<=\().+?(?=\))";
+
         public static string ParseAttribute(MethodCallExpression me)
         {
             var containsExpr = me.Arguments.Single();
@@ -54,6 +58,149 @@ namespace LambdaToXpath
             }
 
             return null;
+        }
+
+        public static bool ParseSiblings(Element element, string expressionPart)
+        {
+            if (expressionPart.Contains(".FollowingSibling.Name") == true)
+            {
+                element.Siblings.Add(new Sibling() { Name = GetValue(expressionPart) });
+                return true;
+            }
+            else if (expressionPart.Contains(".PrecedingSibling.Name") == true)
+            {
+                element.Siblings.Add(new Sibling() { Name = GetValue(expressionPart), Preceding = true });
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool ParseContextElement(Element element, string expressionPart)
+        {
+            if (expressionPart.Contains(".TargetElementName") == true)
+            {
+                element.TargetElementName = GetValue(expressionPart);
+                return true;
+            }
+            else if (Regex.IsMatch(expressionPart, "Attribute(.*)\\.Text") == true)
+            {
+                var keyVal = GetAttributeNameAndValue(expressionPart);
+                element.Attributes.Add(new Model.Attribute(keyVal.Key) { Text = CleanUp(keyVal.Value), ExactMatch = true });
+                return true;
+            }
+            else if (Regex.IsMatch(expressionPart, "Attribute(.*)\\.Contains") == true)
+            {
+                var keyVal = GetAttributeNameAndContainedText(expressionPart);
+                element.Attributes.Add(new Model.Attribute(keyVal.Key) { Text = CleanUp(keyVal.Value) });
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool ParseRelatives(Element element, string expressionPart)
+        {
+            if (expressionPart.Contains(".Descendant") == true)
+            {
+                element.Relatives.Add(new Relative() { Descendant = true, Name = GetValue(expressionPart) });
+                return true;
+            }
+
+            if (expressionPart.Contains(".Ancestor") == true)
+            {
+                element.Relatives.Add(new Relative() { Descendant = false, Name = GetValue(expressionPart) });
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool ParsePosition(Element element, string expressionPart)
+        {
+            if (expressionPart.Contains(".Position") == true)
+            {
+                var matches = Regex.Matches(expressionPart.Trim('('), extractMethodArguments);
+                element.Position = Int32.Parse(CleanUp(matches[0].Value));
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool ParseParent(Element element, string expressionPart)
+        {
+            if (expressionPart.Contains("Parent.Name") == true)
+            {
+                EnsureParent(element);
+                element.Parent.Name = GetValue(expressionPart);
+                return true;
+            }
+            else if (Regex.IsMatch(expressionPart, "Parent.Attribute(.*)\\.Contains") == true)
+            {
+                EnsureParent(element);
+                var keyVal = GetAttributeNameAndContainedText(expressionPart);
+                element.Parent.Attributes.Add(new Model.Attribute(keyVal.Key) { Text = CleanUp(keyVal.Value) });
+                return true;
+            }
+            else if (Regex.IsMatch(expressionPart, "Parent.Attribute(.*)\\.Text") == true)
+            {
+                EnsureParent(element);
+                var keyVal = GetAttributeNameAndValue(expressionPart);
+                element.Parent.Attributes.Add(new Model.Attribute(keyVal.Key) { Text = CleanUp(keyVal.Value), ExactMatch = true });
+                return true;
+            }
+            else if (expressionPart.Contains("Parent.Position") == true)
+            {
+                var matches = Regex.Matches(expressionPart.Trim('('), extractMethodArguments);
+                element.Parent.Position = Int32.Parse(CleanUp(matches[0].Value));
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void EnsureParent(Element element)
+        {
+            if (element.Parent == null)
+            {
+                element.Parent = new Parent();
+            }
+        }
+
+        public static KeyValuePair<string, string> GetAttributeNameAndContainedText(string expressionPart)
+        {
+            var matches = Regex.Matches(expressionPart.Trim('('), extractMethodArguments);
+
+            return new KeyValuePair<string, string>(CleanUp(matches[0].Value), CleanUp(matches[1].Value));
+        }
+
+        public static KeyValuePair<string, string> GetAttributeNameAndValue(string expressionPart)
+        {
+            var temp = expressionPart.Split("==".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var attributeName = Regex.Match(expressionPart.Trim(')', '('), extractMethodArguments).Value;
+
+            return new KeyValuePair<string, string>(CleanUp(attributeName), CleanUp(temp[1]));
+        }
+
+        public static string GetValue(string expressionPart)
+        {
+            var temp = expressionPart.Split("==".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            string name = temp[0];
+
+            if (temp.Length == 2)
+            {
+                name = temp[1];
+            }
+
+            return CleanUp(name);
+        }
+
+        public static string CleanUp(string raw)
+        {
+            return raw.Trim().Trim(')').Trim('\\').Trim('"');
         }
     }
 }
--- LambdaToXpath/XpathExpressionParser.cs
+++ LambdaToXpath/XpathExpressionParser.cs
@@ -12,8 +12,6 @@ namespace LambdaToXpath
 {
     public class XpathExpressionParser
     {
-        private static string extractMethodArguments = @"(?<=\().+?(?=\))";
-
         public static void ParseExpression(BinaryExpression chain, Element element)
         {
             if (chain.Left is BinaryExpression)
@@ -90,167 +88,26 @@ namespace LambdaToXpath
 
         public static void Parse(Element element, string expressionPart)
         {
-            bool done = ParseParent(element,expressionPart);
+            bool done = ElementParser.ParseParent(element, expressionPart);
 
             if (done == false)
             {
-                done = ParseContextElement(element, expressionPart);
+                done = ElementParser.ParseContextElement(element, expressionPart);
             }
             if (done == false)
             {
-                done = ParseSiblings(element, expressionPart);
+                done = ElementParser.ParseSiblings(element, expressionPart);
             }
             if (done == false)
             {
-                done = ParsePosition(element, expressionPart);
+                done = ElementParser.ParsePosition(element, expressionPart);
             }
             if (done == false)
             {
-                done = ParseRelatives(element,expressionPart);
+                done = ElementParser.ParseRelatives(element, expressionPart);
             }
         }
 
-        private static bool ParseSiblings(Element element, string expressionPart)
-        {
-            if (expressionPart.Contains(".FollowingSibling.Name") == true)
-            {
-                element.Siblings.Add(new Sibling() { Name = GetValue(expressionPart) });
-                return true;
-            }
-            else if (expressionPart.Contains(".PrecedingSibling.Name") == true)
-            {
-                element.Siblings.Add(new Sibling() { Name = GetValue(expressionPart),Preceding = true });
-                return true;
-            }
-
-            return false;
-        }
-
-        private static bool ParseContextElement(Element element, string expressionPart)
-        {
-            if (expressionPart.Contains(".TargetElementName") == true)
-            {
-                element.TargetElementName = GetValue(expressionPart);
-                return true;
-            }
-            else if (Regex.IsMatch(expressionPart,"Attribute(.*)\\.Text") == true)
-            {
-                var keyVal = GetAttributeNameAndValue(expressionPart);
-                element.Attributes.Add(new Model.Attribute(keyVal.Key) { Text = CleanUp(keyVal.Value),ExactMatch = true });
-                return true;
-            }
-            else if (Regex.IsMatch(expressionPart, "Attribute(.*)\\.Contains") == true)
-            {
-                var keyVal = GetAttributeNameAndContainedText(expressionPart);
-                element.Attributes.Add(new Model.Attribute(keyVal.Key) { Text = CleanUp(keyVal.Value) });
-                return true;
-            }
-
-            return false;
-        }
-
-        private static bool ParseRelatives(Element element, string expressionPart)
-        {
-            if (expressionPart.Contains(".Descendant") == true)
-            {
-                element.Relatives.Add(new Relative() { Descendant = true,Name = GetValue(expressionPart)});
-                return true;
-            }
-
-            if (expressionPart.Contains(".Ancestor") == true)
-            {
-                element.Relatives.Add(new Relative() { Descendant = false, Name = GetValue(expressionPart) });
-                return true;
-            }
-
-            return false;
-        }
-
-        private static bool ParsePosition(Element element, string expressionPart)
-        {
-            if (expressionPart.Contains(".Position") == true)
-            {
-                var matches = Regex.Matches(expressionPart.Trim('('), extractMethodArguments);
-                element.Position = Int32.Parse(CleanUp(matches[0].Value));
-                return true;
-            }
-
-            return false;
-        }
-
-        private static bool ParseParent(Element element, string expressionPart)
-        {
-            if (expressionPart.Contains("Parent.Name") == true)
-            {
-                EnsureParent(element);
-                element.Parent.Name = GetValue(expressionPart);
-                return true;
-            }
-            else if (Regex.IsMatch(expressionPart, "Parent.Attribute(.*)\\.Contains") == true)
-            {
-                EnsureParent(element);
-                var keyVal = GetAttributeNameAndContainedText(expressionPart);
-                element.Parent.Attributes.Add(new Model.Attribute(keyVal.Key) { Text = CleanUp(keyVal.Value) });
-                return true;
-            }
-            else if (Regex.IsMatch(expressionPart, "Parent.Attribute(.*)\\.Text") == true)
-            {
-                EnsureParent(element);
-                var keyVal = GetAttributeNameAndValue(expressionPart);
-                element.Parent.Attributes.Add(new Model.Attribute(keyVal.Key) { Text = CleanUp(keyVal.Value), ExactMatch = true });
-                return true;
-            }
-            else if (expressionPart.Contains("Parent.Position") == true)
-            {
-                var matches = Regex.Matches(expressionPart.Trim('('), extractMethodArguments);
-                element.Parent.Position = Int32.Parse(CleanUp(matches[0].Value));
-                return true;
-            }
-
-            return false;
-        }
-
-        private static void EnsureParent(Element element)
-        {
-            if (element.Parent == null)
-            {
-                element.Parent = new Parent();
-            }
-        }
-
-        private static KeyValuePair<string, string> GetAttributeNameAndContainedText(string expressionPart)
-        {
-            var matches = Regex.Matches(expressionPart.Trim('('), extractMethodArguments);
-
-            return new KeyValuePair<string, string>(CleanUp(matches[0].Value), CleanUp(matches[1].Value));
-        }
-
-        private static KeyValuePair<string,string> GetAttributeNameAndValue(string expressionPart)
-        {
-            var temp = expressionPart.Split("==".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-
-            var attributeName = Regex.Match(expressionPart.Trim(')', '('), extractMethodArguments).Value;
-
-            return new KeyValuePair<string, string>(CleanUp(attributeName), CleanUp(temp[1]));
-        }
-
-        private static string GetValue(string expressionPart)
-        {
-            var temp = expressionPart.Split("==".ToCharArray(),StringSplitOptions.RemoveEmptyEntries);
-
-            string name = temp[0];
-
-            if (temp.Length == 2)
-            {
-                name = temp[1];
-            }
-
-            return CleanUp(name);
-        }
-
-        private static string CleanUp(string raw)
-        {
-            return raw.Trim().Trim(')').Trim('\\').Trim('"');
-        }
+      
     }
 }
