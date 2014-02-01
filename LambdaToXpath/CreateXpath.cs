3e443b51bc432857ac523947f1f473640f94d9df
Torgeir <torgeir@helgevold-consulting.com>
2014-02-01 14:55:25 -0500

    Added ability to select properties like element text and attributes

--- LambdaToXpath/CreateXpath.cs
+++ LambdaToXpath/CreateXpath.cs
@@ -8,15 +8,60 @@ using System.Threading.Tasks;
 
 namespace LambdaToXpath
 {
+    public class NonElement
+    {
+        public string Text
+        {
+            get { return "/text()"; }
+        }
+
+        public string Attribute(string name)
+        {
+            return string.Format("@{0}",name.Trim('@'));
+        }
+    }
+
+    public class Where 
+    {
+        public Where(string expr) 
+        {
+            Expr = expr;
+        }
+
+        private string Expr { get; set; }
+
+        public static implicit operator string(Where w)  
+        {
+            return w.Expr;
+        }
+
+        public string Select(Expression<Func<NonElement,string>> expr) 
+        {
+            NonElement nonElement = new NonElement();
+            MemberExpression mex = expr.Body as MemberExpression;
+            if (mex != null && mex.Member.Name == "Text")
+            {
+                return string.Format("{0}{1}",this.Expr,nonElement.Text);
+            }
+            else 
+            {
+                MethodCallExpression me = (MethodCallExpression)expr.Body;
+                var val = Expression.Lambda(me.Arguments[0]).Compile().DynamicInvoke().ToString();
+                return string.Format("{0}/@{1}", this.Expr, val);
+            }
+       }
+    }
+
     public class CreateXpath
     {
-        public static string Where(Expression<Func<Element, bool>> exp)
+        public static Where Where(Expression<Func<Element, bool>> exp)
         {
             Element element = new Element();
 
             LambdaExpressionParser.ParseExpression((BinaryExpression)exp.Body, element);
 
-            return XpathGenerator.ToXpathString(element);
+            return new Where(XpathGenerator.ToXpathString(element));
         }
+
     }
 }
---
+++ UnitTests/Expressions/NonElementReturnsTests.cs
@@ -0,0 +1,51 @@
+﻿using LambdaToXpath;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Expressions
+{
+    [TestClass]
+    public class NonElementReturnsTests
+    {
+        [TestMethod]
+        public void Will_Return_Text_Property()
+        {
+            var xpath = CreateXpath.Where(e => e.TargetElementName == "td").Select(e => e.Text);
+
+            Assert.AreEqual("//td/text()", xpath);
+        }
+
+        [TestMethod]
+        public void Will_Return_Attribute_Property1()
+        {
+            var xpath = CreateXpath.Where(e => e.TargetElementName == "td" && e.Attribute("class").Text == "myClass").Select(e => e.Attribute("class"));
+
+            Assert.AreEqual("//td[@class='myClass']/@class", xpath);
+        }
+
+        [TestMethod]
+        public void Will_Return_Attribute_Property2()
+        {
+            string classAttr = "class";
+            var xpath = CreateXpath.Where(e => e.TargetElementName == "td").Select(e => e.Attribute(classAttr));
+            Assert.AreEqual("//td/@class", xpath);
+        }
+
+        [TestMethod]
+        public void Will_Return_Attribute_Property3()
+        {
+            var xpath = CreateXpath.Where(e => e.TargetElementName == "td").Select(e => e.Attribute(GetAttributeName()));
+            Assert.AreEqual("//td/@class", xpath);
+        }
+
+        private string GetAttributeName() 
+        {
+            string classAttr = "class";
+            return classAttr;
+        }
+    }
+}
