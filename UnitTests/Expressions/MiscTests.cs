dad9eae8ed157d99cc6cae108816d5b542d748e7
Torgeir <torgeir@helgevold-consulting.com>
2014-02-21 21:05:17 -0500

    Added test

---
+++ UnitTests/Expressions/MiscTests.cs
@@ -0,0 +1,23 @@
+﻿using LambdaToXpath;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Expressions
+{
+    [TestClass]
+    public class MiscTests
+    {
+        [TestMethod]
+        public void Partial_Text_And_Partial_Attribute()
+        {
+            var xpath = CreateXpath.Where(e => e.TargetElementName == "li" && e.TargetElementText.Contains("text1234") && e.Attribute("class").Contains("myClass"));
+
+            Assert.AreEqual("//li[contains(@class,'myClass') and contains(text(),'text1234')]", xpath);
+        }
+
+    }
+}
